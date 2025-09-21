6d44847a5071a022dac2f4b4187f0bea6178edc2
ike <ike.emanuel14@gmail.com>
2025-09-20 22:15:30 -0300

    actualizacion codigo streamlit02

--- Proyecto1_lol/visualizacion/app_prueba.py
+++ Proyecto1_lol/visualizacion/app_prueba.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-
+from io import StringIO
+import requests
 
 
 st.title('Streamlit LoL DataProyect')
@@ -9,7 +10,9 @@ def load_data(path:str):
     data = pd.read_csv(path,sep=";")
     return data
 
-df = load_data("./visualizacion.csv")
+url = "https://raw.githubusercontent.com/ike-mercado-huanaque/Random-DataScience/refs/heads/master/Proyecto1_lol/visualizacion/visualizacion.csv"
+response = requests.get(url)
+df = load_data(StringIO(response.text))
 st.write(df)
 
 
