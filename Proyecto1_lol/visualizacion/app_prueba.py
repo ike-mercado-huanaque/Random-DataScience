13e42d35a90917ef77df452407d37fb8b4afd95a
ike <ike.emanuel14@gmail.com>
2025-09-20 21:27:31 -0300

    se actualizo visualizacion y corrigio codigo streamlit

--- Proyecto1_lol/visualizacion/app_prueba.py
+++ Proyecto1_lol/visualizacion/app_prueba.py
@@ -1,16 +1,7 @@
 import streamlit as st
-import pymysql
 import pandas as pd
 
-conn = pymysql.connect(
-    host='localhost',
-    user='root',
-    password='password',
-    database='database_name'
-)
-
-sql_query = "SELECT \nidpartida,puuid,championId,championName,win\nFROM \npartidas\nWHERE\nidpartida IN (SELECT idpartida \nFROM partidas \nWHERE TRIM(riotIdGameName) = TRIM('EDUPOIO')\n)"
-df = pd.read_sql(sql_query, conn)
+df = pd.read_csv("visualizacion.csv", sep=";")
 
 st.title('My app :)')
 st.dataframe(df)
@@ -19,4 +10,3 @@ st.line_chart(df['championId'])
 st.bar_chart(df['championId'])
 
 
-conn.close()
