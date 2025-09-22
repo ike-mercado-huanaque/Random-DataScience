ab2bf60e8bc06696557c53599e5ce1656e0aa50f
ike <ike.emanuel14@gmail.com>
2025-09-22 12:25:14 -0300

    se agrega markdown test3

--- Proyecto1_lol/visualizacion/app_prueba.py
+++ Proyecto1_lol/visualizacion/app_prueba.py
@@ -50,20 +50,19 @@ frec: Frequence of Appear
 
 AllyHelpScore''')
 
-st.latex('''
-
-$$ AllyHelpScore = WinRate(AllyChamp)\times frec(AllyChamp)$$
+st.latex(r'''
+$$AllyHelpScore = WinRate(AllyChamp)\times frec(AllyChamp)$$
 $$AllyHelpScore = \frac{wins(AllyChamp)}{frec(AllyChamp)}\times\frac{frec(AllyChamp)}{TotalMatches}$$
 ''')
 st.markdown("AllyUselessScore ")
 
-st.latex(''' 
+st.latex(r''' 
 $$ AllyUselessScore = LoseRate(AllyChamp)\times frec(AllyChamp)$$
 $$AllyUselessScore = \frac{losses(AllyChamp)}{frec(AllyChamp)}\times\frac{frec(AllyChamp)}{TotalMatches}$$
 ''')
 
 st.markdown("Utility")
-st.latex("$$Utility = AllyHelpScore - AllyUselessScore$$ ")
+st.latex(r"$$Utility = AllyHelpScore - AllyUselessScore$$ ")
 
 st.markdown("Los siguientes gráficos muestran los AllyHelpScore, AllyUselessScore y Utility de un jugador seleccionado al azar. Si dicho jugador le tocara un campeon con alta utilidad, puede ser aconsejable que trate de hacer sinergia con dicho aliado. Sin embargo si le toca un campeon con pesima utilidad, puede decidir o bien banearlo o bien dodgear la partida.")
 
