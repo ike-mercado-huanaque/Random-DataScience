44e46b1ee4291482a6d0fe6f5e6db798979f9855
ike <ike.emanuel14@gmail.com>
2025-09-22 02:37:47 -0300

    cambio streamlitapp v10

--- Proyecto1_lol/visualizacion/app_prueba.py
+++ Proyecto1_lol/visualizacion/app_prueba.py
@@ -37,7 +37,7 @@ pd.melt(df_scores[['championName','AllyHelp_Score','AllyUseless_Score','utility'
 
 #grafico
 
-col1, col2 = st.columns([2,1])
+col1, col2 = st.columns([2,2])
 
 with col1:
     st.header("Grafico de scores")
